aa5d804fb99075e2995c381acccdb90a096aa572
Bot Wea ID <91173360+botwea-ID@users.noreply.github.com>
2021-09-23 08:42:21 +0700

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,9 +3,9 @@ import GithubIco from '../../Media/github-icon.svg';
 const Footer = () => {
     return (
         <footer>
-            <div className="footer-left">Made with ❤️ by Keshav</div>
+            <div className="footer-left">Made with ❤️ by Franky</div>
             <div className="footer-right">
-                <a href="https://github.com/kecav/igdownloader"  target="_blank" rel="noreferrer">
+                <a href="https://github.com/Franky404"  target="_blank" rel="noreferrer">
                     <img src={GithubIco} className="github-icon" alt="Github redirect"/>
                 </a>
             </div>
